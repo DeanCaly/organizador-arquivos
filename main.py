fe3aea6956e254daf220a5809d754203f0a19c4d
DeanCaly <deancaly@gmail.com>
2026-06-07 20:55:48 -0300

    feat: classificar arquivo por categoria

--- main.py
+++ main.py
@@ -1,8 +1,32 @@
 from pathlib import Path
 
-pasta = Path('pasta_teste')
+categorias = {
+    'Imagens': ['.jpg', '.jpeg', '.png', '.gif'],
+    'Documentos' : ['.pdf', '.docx', '.txt'],
+    'Áudios' : ['.mp3', '.wav', '.ogg'],
+    'Vídeos' : ['.mp4', '.mkv', '.mov'],
+    'Planilhas' : ['.xlsx', '.csv'],
+}
 
-for arquivo in pasta.iterdir():
-    if arquivo.is_file():
-        print(f'Arquivo: {arquivo.name}')
-        print(f'Extensão: {arquivo.suffix}')
+def mover_arquivo():
+    pass
+
+def obter_categoria(extensao):
+    for categoria, extensoes in categorias.items():
+        if extensao in extensoes:
+            return categoria
+    
+    return 'Outros'
+    
+
+def listar_arquivos(pasta):
+    
+    p = Path(pasta)
+    for arquivo in p.iterdir():
+        if arquivo.is_file():
+            catergoria = obter_categoria(arquivo.name, arquivo.suffix)
+            print(f'{arquivo.name} - {catergoria}')
+
+            
+listar_arquivos('pasta_teste')
+
